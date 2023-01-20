8846e301630b4f10e3f798a1282d665816dd1566
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-20 17:18:57 +0200

    Created user repository

---
+++ src/main/java/richblog/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package richblog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import richblog.models.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+}
