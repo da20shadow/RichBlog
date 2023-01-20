cc955bb48d2381f4efdf466ad7f7ec9b1e2aa5a5
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-20 17:11:31 +0200

    Created Roles Entity

---
+++ src/main/java/richblog/models/entities/Role.java
@@ -0,0 +1,20 @@
+package richblog.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role extends BaseEntity{
+    @Column(nullable = false)
+    private String name;
+}
