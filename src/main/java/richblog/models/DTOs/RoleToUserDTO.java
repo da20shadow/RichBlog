144e9c4eabc64ed48884a3908b17b8466cf7d46b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-21 23:56:35 +0200

    Created Role To User DTO

---
+++ src/main/java/richblog/models/DTOs/RoleToUserDTO.java
@@ -0,0 +1,22 @@
+package richblog.models.DTOs;
+
+public class RoleToUserDTO {
+    private String username;
+    private String roleName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
